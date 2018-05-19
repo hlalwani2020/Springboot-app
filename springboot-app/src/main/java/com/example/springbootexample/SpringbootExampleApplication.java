9e8d3d7aeee8a7d1b0210795e2b22ccf98df02f0
smartyrad <smarita.sharma2015@vit.ac.in>
2018-05-19 17:00:29 +0530

    To understand how springboot works

---
+++ springboot-app/src/main/java/com/example/springbootexample/SpringbootExampleApplication.java
@@ -0,0 +1,24 @@
+package com.example.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExampleApplication.class, args);
+    }
+
+    @RequestMapping(value = "/Hello", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello everyone! This is sample number 1!";
+    }
+
+
+
+}
